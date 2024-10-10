276c0d32111d2ee509aa4e59b396e35c1b021504
pijije <davillatoro@scl.edu.gt>
2024-10-10 11:54:30 -0600

    main realizado

---
+++ main.js
@@ -0,0 +1,20 @@
+
+let DOM = document.querySelector("#root");
+
+
+let componentes_1 = document.createElement('div');
+componentes_1.className = "componentes1";
+
+DOM.appendChild(componentes_1);
+
+
+let componentes_2 = document.createElement('div');
+componentes_2.className = "componentes2";
+DOM.appendChild(componentes_2);
+
+
+let componentes_3 = document.createElement('div');
+componentes_3.className = "componentes3";
+DOM.appendChild(componentes_3);
+
+
